668ff4ab7a0d0dc3b25acdc658653c13f730e40d
He55 <luck.he55@gmail.com>
2019-03-09 14:49:47 +0800

    Initial commit

---
+++ AnimatedDesktop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AnimatedDesktop
+//
+//  Created by 何伟忠 on 2019/3/9.
+//  Copyright © 2019 hummer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
