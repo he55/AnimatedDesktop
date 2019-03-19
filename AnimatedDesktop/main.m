748f12daa0936ed6e597af69a46a3698b1e2295a
He55 <luck.he55@gmail.com>
2019-03-20 00:10:21 +0800

    后台启动程序不再验证参数

--- AnimatedDesktop/main.m
+++ AnimatedDesktop/main.m
@@ -14,12 +14,20 @@ int main(int argc, const char * argv[]) {
         // insert code here...
         NSString *flags = @"--make";
         
-        if (argc < 2) {
-             NSLog(@"传入的参数太少。");
-            return -1;
-        }
-        
         if (![flags isEqualToString:@(argv[argc - 1])]) {
+            if (argc < 2) {
+                NSLog(@"传入的参数太少。");
+                return -1;
+            }
+            
+            NSString *filePath = @(argv[1]);
+            if (![[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+                NSLog(@"文件 \"%@\" 不存在。", filePath);
+                return -1;
+            }
+            
+            NSLog(@"AnimatedDesktop is background run!");
+            
             NSMutableArray<NSString *> *args = [NSMutableArray new];
             for (int i = 1; i < argc; i++) {
                 [args addObject:@(argv[i])];
@@ -34,19 +42,10 @@ int main(int argc, const char * argv[]) {
             return 0;
         }
         
-        NSString *filePath = @(argv[1]);
-        
-        if (![[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
-            NSLog(@"文件 \"%@\" 不存在。", filePath);
-            return -1;
-        }
-        
-        NSLog(@"AnimatedDesktop is run!");
-        
         // 创建动态桌面类
+        NSString *filePath = @(argv[1]);
         HUMApplication *app = [HUMApplication applicationWithVideoPath:filePath];
-        
-        if (argc >= 3) {
+        if (argc > 3) {
             NSString *mutedString = @(argv[2]);
             app.muted = [mutedString boolValue];
         }
