2d79913e2af7ac387020d755208d353a1ed39c14
He55 <luck.he55@gmail.com>
2019-03-20 22:04:19 +0800

    添加进程单例模式

--- AnimatedDesktop/main.m
+++ AnimatedDesktop/main.m
@@ -9,10 +9,12 @@
 #import <Foundation/Foundation.h>
 #import "HUMApplication.h"
 
+static void killapp(void);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSString *flags = @"--make";
+        NSString *flags = @"--AnimatedDesktop=1100";
         
         if (![flags isEqualToString:@(argv[argc - 1])]) {
             if (argc < 2) {
@@ -35,22 +37,49 @@ int main(int argc, const char * argv[]) {
             [args addObject:flags];
             
             NSTask *task = [NSTask new];
-            task.launchPath = @(argv[0]);
+            task.executableURL = [NSURL fileURLWithPath:@(argv[0])];
             task.arguments = args;
             [task launch];
             
             return 0;
         }
         
+        // 防止程序运行多个
+        killapp();
+        
         // 创建动态桌面类
         NSString *filePath = @(argv[1]);
         HUMApplication *app = [HUMApplication applicationWithVideoPath:filePath];
         if (argc > 3) {
             NSString *mutedString = @(argv[2]);
-            app.muted = [mutedString boolValue];
+            app.muted = mutedString.boolValue;
         }
         
         [app runAnimatedDesktop];
     }
     return 0;
+}
+
+static void killapp(void) {
+    pid_t cpid = getpid();
+    NSPipe *pipe = [NSPipe new];
+    NSTask *task = [NSTask new];
+    task.executableURL = [NSURL fileURLWithPath:@"/bin/bash"];
+    task.arguments = @[@"-c", @"ps axu | grep 'AnimatedDesktop=1100' | grep -v 'grep' | awk '{print $2}'"];
+    task.standardOutput = pipe;
+    
+    [task launch];
+    [task waitUntilExit];
+    
+    NSFileHandle *fileHandle = pipe.fileHandleForReading;
+    NSData *data = [fileHandle readDataToEndOfFile];
+    NSString *pidString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    for (NSString *pString in [pidString componentsSeparatedByString:@"\n"]) {
+        int opid = pString.intValue;
+        if (opid != 0 && opid != cpid) {
+            // 结束进程
+            kill(opid, SIGKILL);
+        }
+    }
 }
